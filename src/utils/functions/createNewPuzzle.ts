ecc5b026dd3306980055e548312d49d52b55af68
Tal Kohavy <talkohavy@gmail.com>
2024-10-26 09:21:23 +0300

    created createNewPuzzle

---
+++ src/utils/functions/createNewPuzzle.ts
@@ -0,0 +1,8 @@
+import { GOAL_BOARD } from '../constants.js';
+import { toShuffledArray } from './toShuffledArray.js';
+
+export function createNewPuzzle() {
+  const puzzle = toShuffledArray(GOAL_BOARD); // [5, 7, 3, 0, 6, 2, 1, 4, 8];
+
+  return puzzle;
+}
