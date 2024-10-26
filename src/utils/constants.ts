218c6abbd6ea9b1e25ce8a703e692ea835592887
Tal Kohavy <talkohavy@gmail.com>
2024-10-26 09:19:09 +0300

    created the constant of GOAL_BOARD

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,8 @@ const CUBE_SIZE = 3;
 const TILES_COUNT = CUBE_SIZE ** 2;
 const SPACE_VALUE = 0;
 
-export { TILES_COUNT, CUBE_SIZE, SPACE_VALUE };
+const GOAL_BOARD = Array.from(Array(TILES_COUNT).keys());
+GOAL_BOARD.shift();
+GOAL_BOARD.push(0);
+
+export { GOAL_BOARD, TILES_COUNT, CUBE_SIZE, SPACE_VALUE };
