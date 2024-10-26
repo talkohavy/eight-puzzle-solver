be27be3b3c0d5a4e788a47c73d5d964f67e6385a
Tal Kohavy <talkohavy@gmail.com>
2024-10-26 12:34:15 +0300

    removed function from types

--- src/models/EightTilesPuzzle/types.ts
+++ src/models/EightTilesPuzzle/types.ts
@@ -22,26 +22,4 @@ export type BoardState = {
   spacePosition: SpacePosition;
 };
 
-type CreateBoardStateProps = {
-  board: Array<number>;
-  parent: BoardState;
-  g: number;
-  h: number;
-  spacePosition: SpacePosition;
-};
-
-export function createBoardState(props: CreateBoardStateProps): BoardState {
-  const { board, parent, g, h, spacePosition } = props;
-
-  return {
-    id: board.join(','),
-    board,
-    parent,
-    g,
-    h,
-    f: g + h,
-    spacePosition,
-  };
-}
-
 export type Board = Array<number>;
