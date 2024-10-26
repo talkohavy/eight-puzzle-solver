a00f28d6395dc612f4aee9e6908d0e5251e8ffb5
Tal Kohavy <talkohavy@gmail.com>
2024-10-26 11:22:31 +0300

    renamed isGoal to isGoalReached

--- src/models/EightTilesPuzzle/logic/utils/isGoalReached.ts
+++ src/models/EightTilesPuzzle/logic/utils/isGoalReached.ts
@@ -1,5 +1,5 @@
 import { GOAL_BOARD } from '../constants.js';
 
-export function isGoal(currentBoard: Array<number>) {
+export function isGoalReached(currentBoard: Array<number>) {
   return currentBoard.every((value, index) => value === GOAL_BOARD[index]);
 }
