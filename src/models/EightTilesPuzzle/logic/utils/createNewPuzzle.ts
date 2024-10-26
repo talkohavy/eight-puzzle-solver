234be661dbfe6af9f8003b92ea78af5f2b4c5a02
Tal Kohavy <talkohavy@gmail.com>
2024-10-26 11:21:08 +0300

    createdNewPuzzle amended

--- src/models/EightTilesPuzzle/logic/utils/createNewPuzzle.ts
+++ src/models/EightTilesPuzzle/logic/utils/createNewPuzzle.ts
@@ -1,7 +1,8 @@
 import { GOAL_BOARD } from '../constants.js';
 import { toShuffledArray } from '../../../../utils/functions/toShuffledArray.js';
+import { Board } from '../../types.js';
 
-export function createNewPuzzle() {
+export function createNewPuzzle(): Board {
   const puzzle = toShuffledArray(GOAL_BOARD); // [5, 7, 3, 0, 6, 2, 1, 4, 8];
 
   return puzzle;
